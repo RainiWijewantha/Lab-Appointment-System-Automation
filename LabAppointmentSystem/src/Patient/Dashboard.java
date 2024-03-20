cf5353181e697f390a03fa87cdd20c92791760fe
RainiWijewantha <112390022+RainiWijewantha@users.noreply.github.com>
2024-03-21 00:44:03 +0530

    Automation code init commit

---
+++ LabAppointmentSystem/src/Patient/Dashboard.java
@@ -0,0 +1,41 @@
+package Patient;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Dashboard {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "E:\\ICBT - Bsc\\Automation\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+		Thread.sleep(5000);
+
+		String baseURL = "http://localhost:9090";
+		Thread.sleep(5000);
+
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		Thread.sleep(5000);
+
+		WebDriver chromeDriver = new ChromeDriver(option);
+		Thread.sleep(5000);
+
+		chromeDriver.manage().window().maximize();
+		Thread.sleep(5000);
+
+		chromeDriver.get(baseURL);
+		Thread.sleep(5000);
+		
+		WebElement displayAppointmentPage = chromeDriver.findElement(By.xpath("(//a[contains(text(),'Request')])[1]"));
+		displayAppointmentPage.click();
+		Thread.sleep(5000);
+		
+		
+	}
+	
+	
+
+}
