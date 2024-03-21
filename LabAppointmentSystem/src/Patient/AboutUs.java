a8d1f8200fbcd829f43e0c1a8f2e5d1aec678f79
RainiWijewantha <112390022+RainiWijewantha@users.noreply.github.com>
2024-03-21 19:21:47 +0530

    Changes added

--- LabAppointmentSystem/src/Patient/AboutUs.java
+++ LabAppointmentSystem/src/Patient/AboutUs.java
@@ -1,9 +1,35 @@
 package patient;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class AboutUs {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "E:\\ICBT - Bsc\\Automation\\Chrome Driver\\chromedriver-win64\\chromedriver.exe");
+		Thread.sleep(5000);
+
+		String baseURL = "http://localhost:9090";
+
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		Thread.sleep(5000);
+
+		WebDriver chromeDriver = new ChromeDriver(option);
+		Thread.sleep(5000);
+
+		chromeDriver.manage().window().maximize();
+		Thread.sleep(5000);
+
+		chromeDriver.get(baseURL);
+		Thread.sleep(5000);
+		
+		WebElement about_us = chromeDriver.findElement(By.xpath("//a[normalize-space()='About Us']"));
+		about_us.click();
 
 	}
 
